967e9e1a8ff0336aa079b8bada76ce79aa725ae9
Zhaowei Si <zsi@g.clemson.edu>
2022-10-24 12:39:31 -0700

    update navigation bar styles

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,19 +1,17 @@
 <template>
-  <header class="p-3 text-bg-dark">
     <div class="container">
-      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="#" class="nav-link px-2 text-white">Home</a></li>
+          <li><a href="#" class="nav-link px-2 link-dark">Home</a></li>
         </ul>
 
         <div class="text-end">
-          <button type="button" class="btn btn-outline-light me-2">Login</button>
-          <button type="button" class="btn btn-warning">Sign-up</button>
+          <button type="button" class="btn btn-outline-primary me-2">Login</button>
+          <button type="button" class="btn btn-primary">Sign-up</button>
         </div>
       </div>
     </div>
-  </header>
 </template>
 
 <script>
