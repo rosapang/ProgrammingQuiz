2fc09cd952220de10f366b7e64a43cfa411d3782
Zhaowei Si <zsi@g.clemson.edu>
2022-10-24 13:43:17 -0700

    refactor-move router function from main.js to index.js under router folder

--- client/src/main.js
+++ client/src/main.js
@@ -1,30 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 //import Vue from 'vue'
-import {createRouter, createWebHistory} from 'vue-router'
-import LogIn from '@/views/LogIn.vue'
-
-// 1. Define route components.
-// These can be imported from other files
-const Home = { template: '<div>Home</div>' }
-
-// 2. Define some routes
-// Each route should map to a component.
-// We'll talk about nested routes later.
-const routes = [
-  { path: '/', component: Home },
-  { path: '/login', component: LogIn},
-]
-
-// 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
-// keep it simple for now.
-const router = createRouter({
-  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
-  history: createWebHistory(process.env.BASE_URL),
-  routes, // short for `routes: routes`
-})
-
+import router from './router';
 
 // import axios
 import axios from 'axios'
---
+++ client/src/router/index.js
@@ -0,0 +1,23 @@
+import {createRouter, createWebHistory} from 'vue-router'
+
+// 1. Define route components.
+// These can be imported from other files
+import LogIn from '@/views/LogIn.vue'
+
+// 2. Define some routes
+// Each route should map to a component.
+// We'll talk about nested routes later.
+const routes = [
+  { path: '/login', component: LogIn},
+  ]
+  
+  // 3. Create the router instance and pass the `routes` option
+  // You can pass in additional options here, but let's
+  // keep it simple for now.
+  const router = createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: createWebHistory(process.env.BASE_URL),
+    routes, // short for `routes: routes`
+  })
+
+export default router
