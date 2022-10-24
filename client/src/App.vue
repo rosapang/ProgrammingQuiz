aa6efd543e4b51089c9e26f71f457ea3a71c365a
Zhaowei Si <zsi@g.clemson.edu>
2022-10-24 13:28:25 -0700

    add component LogIn to the main page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,21 +1,6 @@
 <template>
   <NavBar />
-  <main class="form-signin w-100 m-auto">
-  <form>
-    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-
-    <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-      <label for="floatingInput">Email address</label>
-    </div>
-    <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-      <label for="floatingPassword">Password</label>
-    </div>
-
-    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-  </form>
-  </main>
+  <LogIn />
 </template>
 
 <script>
@@ -28,9 +13,10 @@
 //   }
 // }
 import NavBar from '@/components/NavBar.vue'
+import LogIn from '@/views/LogIn.vue'
 
 export default {
-  components: {NavBar}
+  components: {NavBar, LogIn}
 }
 </script>
 
