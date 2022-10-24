41ef9ada85645aa424e3e23cb3fbd7e337691442
Zhaowei Si <zsi@g.clemson.edu>
2022-10-23 21:36:44 -0700

    add sign in forms from bootstrap template

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,20 @@
 <template>
-  <div id="app">
-    <p>
-    <!-- <ClientTasks /> -->
-    <router-link to="/">Go to Home</router-link>
-    <router-link to="/about">Go to About</router-link>
-    </p>
-  <!-- route outlet -->
-  <!-- component matched by the route will render here -->
-  <router-view></router-view>
-  </div>
+  <main class="form-signin w-100 m-auto">
+  <form>
+    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+    <div class="form-floating">
+      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <label for="floatingInput">Email address</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+      <label for="floatingPassword">Password</label>
+    </div>
+
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+  </form>
+  </main>
 </template>
 
 <script>
